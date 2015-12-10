17ef226cfc4a47416a89b73a49537cc29ecb5106
dancmj <daniel.c82@gmail.com>
2015-12-09 19:57:23 -0700

    Renamed the shellscript and added more functionality

--- imagesnap.sh
+++ imagesnap.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+FOLDER_NAME='wake_up'
+
 YEAR=$(date '+%Y')
 MONTH=$(date '+%m_%b')
 DAY=$(date '+%d_%a')
 TIME=$(date '+%H.%M.%S %p')
 
 FILENAME="$TIME"
-TARGET_DIRECTORY="/Users/$USER/Pictures/wake_up/$YEAR/$MONTH/$DAY"
+TARGET_DIRECTORY="/Users/$USER/Pictures/$FOLDER_NAME/$YEAR/$MONTH/$DAY"
 
 mkdir -p $TARGET_DIRECTORY
 
